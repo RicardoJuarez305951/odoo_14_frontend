69b33dbe77c4f1c7cd0679e9e82c9e9f3c2a5511
Ricardo <ricardojuarezflores01@gmail.com>
2026-06-22 00:21:30 -0600

    [ADD] Mejora de gráficas de pie

--- addons/ventas_chart/static/src/js/ventas.js
+++ addons/ventas_chart/static/src/js/ventas.js
@@ -10,43 +10,77 @@ odoo.define("ventas_chart.chart", function (require) {
 
     var VentasChart = AbstractAction.extend({
         template: "template_ventas_chart",
+        events:{
+            "change .sale_state":"change_sale_state",
+        },
         start: function () {
             var self = this;
+            self.sale_chart = undefined
+            self.lines = []
             self._super()
             core.bus.on("DOM_updated", this, function () {
-                self.renderChart()
+                self.fetch_group_by_state().then(function(lines){
+                    self.lines = lines || []
+                    self.renderChart(self.getFilteredLines())
+                })
             })
         },
-        renderChart: function () {
-            this._rpc({
+
+        fetch_group_by_state:function(){
+            return this._rpc({
                 model: "sale.order",
                 method: "group_by_state",
                 args: [],
                 kwargs: {}
-            }).then(function (res) {
-                console.log(res)
-                const ctx = document.getElementById("myChart").getContext("2d");
-
-                var myChart = new Chart(ctx, {
-                    type: "bar",
-                    data: {
-                        labels: _.map(res, function (el) { return states[el.state] || el.state }),
-                        datasets: [{
-                            label: "Ventas por Estado",
-                            data: _.map(res, function (el) { return el.sum }),
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            y: {
-                                beginAtZero: true
-                            }
-                        }
-                    }
-                });
-
             })
-        }
+        },
+        getFilteredLines: function () {
+            var enabledStates = _.map(
+                _.filter($(".sale_state"), function (el) { return $(el).is(":checked") }),
+                function (el) { return $(el).data("name") }
+            )
+
+            return _.filter(this.lines, function (line) {
+                return enabledStates.indexOf(line.state) >= 0
+            })
+        },
+        renderChart: function (lines) {
+            var ctx = document.getElementById("myChart").getContext("2d")
+
+            if (this.sale_chart) {
+                this.sale_chart.destroy()
+            }
+
+            this.sale_chart = new Chart(ctx, {
+                type: "pie",
+                data: {
+                    labels: _.map(lines, function (el) { return states[el.state] || el.state }),
+                    datasets: [{
+                        label: "Ventas por Estado",
+                        data: _.map(lines, function (el) { return el.sum }),
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgb(255, 205, 86)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgb(255, 205, 86)'
+                        ],
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false
+                }
+            })
+        },
+        change_sale_state:function(ev){
+            this.renderChart(this.getFilteredLines())
+        },
     })
 
     core.action_registry.add("ventas_chart", VentasChart)
